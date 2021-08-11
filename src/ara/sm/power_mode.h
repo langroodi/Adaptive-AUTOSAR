6b5c5271a1acd41faf9980bf4e0465de92ae071b
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2021-08-11 20:38:34 +0200

    State Management Power Mode is added: - Power mode request and response message enum is added.

---
+++ src/ara/sm/power_mode.h
@@ -0,0 +1,27 @@
+#ifndef POWER_MODE_H
+#define POWER_MODE_H
+
+namespace ara
+{
+    namespace sm
+    {
+        /// @brief Power mode request message
+        enum class PowerModeMsg
+        {
+            kOn,        ///< Normal operation mode
+            kOff,       ///< Preparation mode for shutdown
+            kSuspend,   ///< Preparation mode for RAM and/or disl suspension
+        };
+
+        /// @brief Power mode response message
+        enum class PowerModeRespMsg
+        {
+            kDone,          ///< Requested power mode reached successfully
+            kFailed,        ///< Requested power mode reaching failed
+            kBusy,          ///< Requested power mode ignored due to busy processing list
+            kNotSupported   ///< Requested power mode not supported
+        };
+    }
+}
+
+#endif
