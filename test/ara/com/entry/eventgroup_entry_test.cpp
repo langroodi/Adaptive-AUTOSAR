cec45a459cd8da7c6bdcecb9c678a033ad22ddba
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2021-08-22 11:32:05 +0200

    Adding Entry unit test: - Factory unit tests are added.

--- test/ara/com/entry/eventgroup_entry_test.cpp
+++ test/ara/com/entry/eventgroup_entry_test.cpp
@@ -7,7 +7,26 @@ namespace ara
     {
         namespace entry
         {
+            TEST(EventgroupEntryTest, SubscribeEventFactory)
+            {
+                const uint16_t cServiceId = 0x0001;
+                const uint16_t cInstanceId = 0x0002;
+                const uint8_t cMajorVersion = 0x003;
+                const uint16_t cEventgroupId = 0x0004;
+                const EntryType cType = EntryType::Subscribing;
+                const uint32_t cTTL = 0x00000000;
 
+                auto _entry =
+                    EventgroupEntry::CreateSubscribeEventEntry(
+                        cServiceId, cInstanceId, cMajorVersion, cEventgroupId);
+                
+                EXPECT_EQ(_entry.ServiceId(), cServiceId);
+                EXPECT_EQ(_entry.InstanceId(), cInstanceId);
+                EXPECT_EQ(_entry.MajorVersion(), cMajorVersion);
+                EXPECT_EQ(_entry.EventgroupId(), cEventgroupId);
+                EXPECT_EQ(_entry.Type(), cType);
+                EXPECT_GT(_entry.TTL(), cTTL);
+            }
         }
     }
 }
--- test/ara/com/entry/service_entry_test.cpp
+++ test/ara/com/entry/service_entry_test.cpp
@@ -7,7 +7,26 @@ namespace ara
     {
         namespace entry
         {
-            
+            TEST(ServiceEntryTest, FindServiceFactory)
+            {
+                const uint16_t cServiceId = 0x0001;
+                const uint32_t cTTL = 0x00000002;
+                const uint16_t cInstanceId = 0x0003;
+                const uint8_t cMajorVersion = 0x004;
+                const uint32_t cMinorVersion = 0x00000005;
+                const EntryType cType = EntryType::Finding;
+
+                auto _entry =
+                    ServiceEntry::CreateFindServiceEntry(
+                        cServiceId, cTTL, cInstanceId, cMajorVersion, cMinorVersion);
+
+                EXPECT_EQ(_entry.ServiceId(), cServiceId);
+                EXPECT_EQ(_entry.TTL(), cTTL);
+                EXPECT_EQ(_entry.InstanceId(), cInstanceId);
+                EXPECT_EQ(_entry.MajorVersion(), cMajorVersion);
+                EXPECT_EQ(_entry.MinorVersion(), cMinorVersion);
+                EXPECT_EQ(_entry.Type(), cType);
+            }
         }
     }
 }
