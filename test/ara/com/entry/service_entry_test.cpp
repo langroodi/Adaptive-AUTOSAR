03a4f5dda8f5b9add9abe3c2d5ce9aa32e24ac23
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2021-08-23 12:35:03 +0200

    ServiceEntry factory unit tests are added.

--- test/ara/com/entry/service_entry_test.cpp
+++ test/ara/com/entry/service_entry_test.cpp
@@ -10,7 +10,7 @@ namespace ara
             TEST(ServiceEntryTest, FindServiceFactory)
             {
                 const uint16_t cServiceId = 0x0001;
-                const uint32_t cTTL = 0x00000002;
+                const uint32_t cTTL = 0x000002;
                 const uint16_t cInstanceId = 0x0003;
                 const uint8_t cMajorVersion = 0x004;
                 const uint32_t cMinorVersion = 0x00000005;
@@ -26,6 +26,48 @@ namespace ara
                 EXPECT_EQ(_entry.MajorVersion(), cMajorVersion);
                 EXPECT_EQ(_entry.MinorVersion(), cMinorVersion);
                 EXPECT_EQ(_entry.Type(), cType);
+            }
+
+            TEST(ServiceEntryTest, OfferServiceFactory)
+            {
+                const uint16_t cServiceId = 0x0001;
+                const uint16_t cInstanceId = 0x0003;
+                const uint8_t cMajorVersion = 0x004;
+                const uint32_t cMinorVersion = 0x00000005;
+                const EntryType cType = EntryType::Offering;
+                const uint32_t cTTL = 0x000000;
+
+                auto _entry =
+                    ServiceEntry::CreateOfferServiceEntry(
+                        cServiceId, cInstanceId, cMajorVersion, cMinorVersion);
+
+                EXPECT_EQ(_entry.ServiceId(), cServiceId);
+                EXPECT_EQ(_entry.InstanceId(), cInstanceId);
+                EXPECT_EQ(_entry.MajorVersion(), cMajorVersion);
+                EXPECT_EQ(_entry.MinorVersion(), cMinorVersion);
+                EXPECT_EQ(_entry.Type(), cType);
+                EXPECT_GT(_entry.TTL(), cTTL);
+            }
+
+            TEST(ServiceEntryTest, StopOfferFactory)
+            {
+                const uint16_t cServiceId = 0x0001;
+                const uint16_t cInstanceId = 0x0003;
+                const uint8_t cMajorVersion = 0x004;
+                const uint32_t cMinorVersion = 0x00000005;
+                const EntryType cType = EntryType::Offering;
+                const uint32_t cTTL = 0x000000;
+
+                auto _entry =
+                    ServiceEntry::CreateStopOfferEntry(
+                        cServiceId, cInstanceId, cMajorVersion, cMinorVersion);
+
+                EXPECT_EQ(_entry.ServiceId(), cServiceId);
+                EXPECT_EQ(_entry.InstanceId(), cInstanceId);
+                EXPECT_EQ(_entry.MajorVersion(), cMajorVersion);
+                EXPECT_EQ(_entry.MinorVersion(), cMinorVersion);
+                EXPECT_EQ(_entry.Type(), cType);
+                EXPECT_EQ(_entry.TTL(), cTTL);
             }
         }
     }
