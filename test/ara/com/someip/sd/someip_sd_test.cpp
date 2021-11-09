da3e7d858d24b4702a8462d7084ef42f836037e6
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2021-11-09 21:30:34 +0100

    Make offer stop waiting longer.

--- test/ara/com/someip/sd/someip_sd_test.cpp
+++ test/ara/com/someip/sd/someip_sd_test.cpp
@@ -70,8 +70,8 @@ namespace ara
                                      OfferStopWaitDuration{static_cast<int>(
                                          // Main main first cycle delay
                                          cCycleOfferDelay *
-                                         // Apply minimum Nyquist–Shannon margin (make the duration twice longer)
-                                         2)}
+                                         // Apply Nyquist–Shannon margin (make the duration thrid times longer)
+                                         3)}
                     {
                     }
                 };
