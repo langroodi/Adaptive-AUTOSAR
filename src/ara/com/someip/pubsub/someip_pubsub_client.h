b602e4642c67f02ad56892be3dead698d32a6928
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2021-11-26 22:05:07 +0100

    Create SOME/IP Pub/Sub client interface

---
+++ src/ara/com/someip/pubsub/someip_pubsub_client.h
@@ -0,0 +1,62 @@
+#ifndef SOMEIP_PUBSUB_CLIENT
+#define SOMEIP_PUBSUB_CLIENT
+
+#include "../../entry/eventgroup_entry.h"
+#include "../../helper/network_layer.h"
+#include "../sd/someip_sd_message.h"
+
+namespace ara
+{
+    namespace com
+    {
+        namespace someip
+        {
+            namespace pubsub
+            {
+                /// @brief SOME/IP publish/subscribe client
+                class SomeIpPubSubClient
+                {
+                private:
+                    helper::NetworkLayer<sd::SomeIpSdMessage> *mCommunicationLayer;
+                    uint8_t mCounter;
+
+                public:
+                    SomeIpPubSubClient() = delete;
+                    ~SomeIpPubSubClient();
+
+                    /// @brief Constructor
+                    /// @param networkLayer Network communication abstraction layer
+                    /// @param counter Counter to make the client distinguishable among other subscribers
+                    SomeIpPubSubClient(
+                        helper::NetworkLayer<sd::SomeIpSdMessage> *networkLayer,
+                        uint8_t counter);
+
+                    /// @brief Try to subscribe to an event-group
+                    /// @param serviceId Service in interest ID
+                    /// @param instanceId Service in interest instance ID
+                    /// @param majorVersion Service in interest major version
+                    /// @param eventgroupId Event-group in interest ID
+                    /// @returns True if the server accept the subscription by sending ACK; otherwise false
+                    bool TrySubscribe(
+                        uint16_t serviceId,
+                        uint16_t instanceId,
+                        uint8_t majorVersion,
+                        uint16_t eventgroupId);
+
+                    /// @brief Unsubscribe from a subscribed event-group
+                    /// @param serviceId Service in interest ID
+                    /// @param instanceId Service in interest instance ID
+                    /// @param majorVersion Service in interest major version
+                    /// @param eventgroupId Event-group in interest ID
+                    void Unsubscribe(
+                        uint16_t serviceId,
+                        uint16_t instanceId,
+                        uint8_t majorVersion,
+                        uint16_t eventgroupId);
+                };
+            }
+        }
+    }
+}
+
+#endif
