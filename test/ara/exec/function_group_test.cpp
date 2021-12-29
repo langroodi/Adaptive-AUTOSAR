fc9c634056fd8d3c86be5779fd9b2f56ad807c87
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2021-12-29 14:45:57 +0100

    :microscope: Add FunctionGroup (in)eqaulity test

--- test/ara/exec/function_group_test.cpp
+++ test/ara/exec/function_group_test.cpp
@@ -30,5 +30,24 @@ namespace ara
 
             EXPECT_TRUE(_otherFunctionGroup.Value().GetInstance() == cShortnamePath);
         }
+
+        TEST(FunctionGroupTest, Equality)
+        {
+            const std::string cShortnamePath{"application"};
+            auto _functionGroup = FunctionGroup::Create(cShortnamePath);
+            auto _otherFunctionGroup = FunctionGroup::Create(cShortnamePath);
+
+            EXPECT_TRUE(_functionGroup.Value() == _otherFunctionGroup.Value());
+        }
+
+        TEST(FunctionGroupTest, Inqquality)
+        {
+            const std::string cShortnamePath{"application"};
+            const std::string cOtherShortnamePath{"otherApplication"};
+            auto _functionGroup = FunctionGroup::Create(cShortnamePath);
+            auto _otherFunctionGroup = FunctionGroup::Create(cOtherShortnamePath);
+
+            EXPECT_TRUE(_functionGroup.Value() != _otherFunctionGroup.Value());
+        }
     }
 }
