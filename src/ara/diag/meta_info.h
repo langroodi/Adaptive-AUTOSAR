9df4b973266c383b0f0b0d2be1b86d4af967c782
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2022-03-28 21:41:41 +0200

    :beer: Define DM MetaInfo based on R21-11: - The MetaInfo interface is documented.

--- src/ara/diag/meta_info.h
+++ src/ara/diag/meta_info.h
@@ -2,30 +2,43 @@
 #define META_INFO_H
 
 #include <stdint.h>
-#include <string_view>
+#include <string>
+#include "../core/optional.h"
 
 namespace ara
 {
+    /// @brief ARA diagnostic management
     namespace diag
     {
-        enum class Context : std::int32_t
+        /// @brief Diagnostic metainfo context
+        enum class Context : uint32_t
         {
-            kDiagnosticCommunication,
-            kFaultMemory,
-            kDoIP
+            kDiagnosticCommunication,   ///< For service request in classic platform Diagnostic Communication Manager (DCM)
+            kFaultMemory,               ///< For Data IDentifier (DID) according to ISO 14229-1 in diagnostic snapshots
+            kDoIP                       ///< For reading Vehicle Identifier Number (VIN) according to ISO 3779
         };
 
+        /// @brief Diagnostic metainfo in different context
         class MetaInfo final
         {
         public:
             MetaInfo() noexcept = delete;
             MetaInfo(const MetaInfo &) = delete;
-            MetaInfo(MetaInfo &&) noexcept = default;
+            MetaInfo(MetaInfo &&) noexcept;
+
             MetaInfo &operator=(const MetaInfo &) = delete;
-            MetaInfo &operator=(MetaInfo &&) &noexcept = default;
-            Context GetValue(std::string_view key) const noexcept;
+            MetaInfo &operator=(MetaInfo &&) &noexcept;
+
+            /// @brief Get an entry value within the metainfo
+            /// @param key Entry key
+            /// @returns Optional entry value based on the key existence
+            core::Optional<std::string> GetValue(std::string key) const noexcept;
+
+            /// @brief Get metainfo context
+            /// @returns Context in which the metainfo has been defined
             Context GetContext() const noexcept;
-            ~MetaInfo() = default;
+
+            ~MetaInfo();
         };
     }
 }
