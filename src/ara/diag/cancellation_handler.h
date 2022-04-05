c236c2c04f0b8cf4fd30fc08786b36bda3c9fd6a
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2022-04-05 22:06:03 +0200

    :beer: Finalize the CancellationHandler interface

--- src/ara/diag/cancellation_handler.h
+++ src/ara/diag/cancellation_handler.h
@@ -7,15 +7,30 @@ namespace ara
 {
     namespace diag
     {
+        /// @brief A wrapper class around a conversation cancellation handler callback
         class CancellationHandler final
         {
+        private:
+            bool mIsCanceled;
+
         public:
             CancellationHandler() = delete;
             CancellationHandler(CancellationHandler &&) noexcept = default;
             CancellationHandler(CancellationHandler &) = delete;
             CancellationHandler &operator=(CancellationHandler &&) noexcept = default;
             CancellationHandler &operator=(CancellationHandler &) = delete;
+
+            /// @brief Get conversation cancellation status
+            /// @returns True if DM canceled the conversation, otherwise false
             bool IsCanceled() const;
+
+            /// @brief Set conversation cancellation status
+            /// @param isCanceled Current conversation cancellation status
+            /// @note The method is not ARA compatible.
+            void SetIsCanceled(bool isCanceled);
+
+            /// @brief Set a callback to be invoked when DM canceled the conversation
+            /// @param notifier A callback to be notified after the cancellation
             void SetNotifier(std::function<void()> notifier);
         };
     }
