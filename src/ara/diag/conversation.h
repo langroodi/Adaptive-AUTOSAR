931219058e16d21f62f267ba15f80084032ba1a1
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2022-06-09 22:29:55 +0200

    :beer: Align Conversation class with R21-11 standard

--- src/ara/diag/conversation.h
+++ src/ara/diag/conversation.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <functional>
 #include <vector>
-#include <string_view>
+#include <string>
 #include "../core/result.h"
 #include "./meta_info.h"
 
@@ -12,51 +12,104 @@ namespace ara
 {
     namespace diag
     {
-        enum class ActivityStatusType : std::uint8_t
+        /// @brief Conversation enable status
+        enum class ActivityStatusType : uint8_t
         {
-            kActive = 0x00,
-            kInactive = 0x01
+            kActive = 0x00,  ///< Active conversation due to a request processing or a non-default session
+            kInactive = 0x01 ///< Inactive conversation
         };
 
-        enum class SessionControlType : std::uint8_t
+        /// @brief Diagnostic session control type
+        enum class SessionControlType : uint8_t
         {
-            kDefaultSession = 0x01,
-            kProgrammingSession = 0x02,
-            kExtendedDiagnosticSession = 0x03,
-            kSafetySystemDiagnosticSession = 0x04
+            kDefaultSession = 0x01,               ///< Default diagnostic session
+            kProgrammingSession = 0x02,           ///< Session to enable memory programming diagnostic services
+            kExtendedDiagnosticSession = 0x03,    ///< Session to enable function adjustment diagnostic services
+            kSafetySystemDiagnosticSession = 0x04 ///< Session to enable safety system diagnostic services
         };
 
-        enum class SecurityLevelType : std::uint8_t
+        /// @brief UDS security level access status
+        enum class SecurityLevelType : uint8_t
         {
-            kLocked = 0x00
+            kLocked = 0x00 ///< Locked security level (no access granted)
         };
 
+        /// @brief Type to identify a conversation
         struct ConversationIdentifierType
         {
-            /* data */
+            /// @brief Conversation ID
+            int64_t id;
         };
 
+        /// @brief A wrapper around the communication between a diagnostic client and a diagnostic server
+        /// @note The class is not fully aligned with the ARA standard.
         class Conversation
         {
         public:
+            /// @brief Conversation factory
+            /// @param metaInfo Additional information to create a conversation
+            /// @returns Reference to the created conversation in case of a valid meta information
+            static ara::core::Result<Conversation &> GetConversation(MetaInfo &metaInfo);
+
+            /// @brief Get all the existing conversations
+            /// @returns Active and inactive conversations collection
+            static std::vector<std::reference_wrapper<Conversation>> GetAllConversations();
+
+            /// @brief Get all the active conversations
+            /// @returns Active conversations collection
+            static std::vector<std::reference_wrapper<Conversation>> GetCurrentActiveConversations();
+
+            /// @brief Activity status property getter
+            /// @returns Active or inactive status
             ara::core::Result<ActivityStatusType> GetActivityStatus();
+
+            /// @brief Register an activity status change callback
+            /// @param notifier Callback to be invoked when the activity status changed
+            /// @returns No error will be returned
             ara::core::Result<void> SetActivityNotifier(
                 std::function<void(ActivityStatusType)> notifier);
+
+            /// @brief Conversation ID property getter
+            /// @returns Conversation ID
             ara::core::Result<ConversationIdentifierType> GetConversationIdentifier();
+
+            /// @brief Diagnostic session property getter
+            /// @returns Coversation active session
             ara::core::Result<SessionControlType> GetDiagnosticSession();
+
+            /// @brief Register a diagnostic session change callback
+            /// @param notifier Callback to be invoked when the diagnostic session changed
+            /// @returns No error will be returned
             ara::core::Result<void> SetDiagnosticSessionNotifier(
                 std::function<void(SessionControlType)> notifier);
-            ara::core::Result<std::string_view> GetDiagnosticSessionShortName(SessionControlType session);
+            
+            /// @brief Convert a given session to a short name
+            /// @param session Session to be converted
+            /// @returns Given session short name
+            ara::core::Result<std::string> GetDiagnosticSessionShortName(
+                SessionControlType session);
+
+            /// @brief Security level property getter
+            /// @returns Status of the active security level
             ara::core::Result<SecurityLevelType> GetDiagnosticSecurityLevel();
+
+            /// @brief Register a security level change callback
+            /// @param notifier Callback to be invoked when the security level status changed
+            /// @returns No error will be returned
             ara::core::Result<void> SetSecurityLevelNotifier(
                 std::function<void(SecurityLevelType)> notifier);
-            ara::core::Result<std::string_view> GetDiagnosticSecurityLevelShortName(SecurityLevelType securityLevel);
-            ara::core::Result<void> ResetToDefaultSession();
 
-            static ara::core::Result<Conversation &> GetConversation(MetaInfo &metaInfo);
-            static std::vector<Conversation &> GetAllConversations();
-            static std::vector<Conversation &> GetCurrentActiveConversations();
+            /// @brief Convert a given security level to a short name
+            /// @param securityLevel Security level to be converted
+            /// @returns Given security level short name
+            ara::core::Result<std::string> GetDiagnosticSecurityLevelShortName(
+                SecurityLevelType securityLevel);
+            
+            /// @brief Reset the ongoing session to default
+            /// @returns No error in case of a successful reset
+            ara::core::Result<void> ResetToDefaultSession();
         };
     }
 }
+
 #endif
