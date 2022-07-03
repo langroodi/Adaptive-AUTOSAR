84ae28374644f885dcfbc497d6b323cfe88e3865
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2022-07-03 16:53:21 +0200

    :beer: Implement SecurityAccess interface: - The interface inherits from the UdsRoutableService; - SecurityAccessis not an abstract class anymore; - Typo fix and interface documentation.

--- src/ara/diag/security_access.h
+++ src/ara/diag/security_access.h
@@ -1,11 +1,12 @@
 #ifndef SECURITY_ACCESS_H
-#define SECUIRTY_ACCESS_H
+#define SECURITY_ACCESS_H
 
 #include <stdint.h>
 #include <future>
 #include <vector>
 #include "../core/instance_specifier.h"
 #include "../core/result.h"
+#include "./routing/routable_uds_service.h"
 #include "./reentrancy.h"
 #include "./meta_info.h"
 #include "./cancellation_handler.h"
@@ -14,27 +15,51 @@ namespace ara
 {
     namespace diag
     {
+        /// @brief Security key comparison result
         enum class KeyCompareResultType
         {
-            kKeyValid = 0x00,
-            kKeyInvalid = 0x01
+            kKeyValid = 0x00,  ///< Valid security key
+            kKeyInvalid = 0x01 ///< Invalid security key
         };
 
-        class SecuirtyAccess
+        /// @brief A class to evaluate the security access to the data among diagnostic services
+        /// @note In contrast with the ARA standard, the class is NOT abstract.
+        class SecurityAccess : routing::RoutableUdsService
         {
+        private:
+            static const uint8_t cSid{0x27};
+
+            const ReentrancyType mReentrancy;
+
         public:
-            explicit SecuirtyAccess(const ara::core::InstanceSpecifier &specifier, ReentrancyType reentrancyType);
-            virtual ~SecuirtyAccess() noexcept = default;
-            virtual std::future<std::vector<std::uint8_t>> GetSeed(
+            /// @brief Constructor
+            /// @param specifier Owner instance specifier
+            /// @param reentrancyType Service reentrancy type
+            explicit SecurityAccess(
+                const ara::core::InstanceSpecifier &specifier,
+                ReentrancyType reentrancyType);
+
+            virtual ~SecurityAccess() noexcept = default;
+
+            /// @brief Request a seed from the client side to provide a security key
+            /// @param securityAccessDataRecord Seed request parameters payload
+            /// @param metaInfo Request message metainfo
+            /// @param cancellationHandler Callback to be invoked when the current conversation is cancelled
+            /// @returns Provided key by the server
+            virtual std::future<std::vector<uint8_t>> GetSeed(
                 std::vector<uint8_t> securityAccessDataRecord,
                 MetaInfo &metaInfo,
-                CancellationHandler cancellationHandler) = 0;
+                CancellationHandler cancellationHandler);
+
+            /// @brief Evaluate the key provided by a client
+            /// @param key Provided key
+            /// @param metaInfo Request message metainfo
+            /// @param cancellationHandler Callback to be invoked when the current conversation is cancelled
+            /// @returns Key evaluation result from the server
             virtual std::future<KeyCompareResultType> CompareKey(
-                std::vector<std::uint8_t> key,
+                std::vector<uint8_t> key,
                 MetaInfo &metaInfo,
-                CancellationHandler cacellationHandler) = 0;
-            ara::core::Result<void> Offer();
-            void StopOffer();
+                CancellationHandler cacellationHandler);
         };
     }
 }
