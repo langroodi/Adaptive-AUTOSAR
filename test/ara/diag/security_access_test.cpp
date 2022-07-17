01bc86be7dbab829a54d2c02f0deb32dc7c0f7a4
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2022-07-17 10:39:34 +0200

    :microscope: Extend SecurityAccess unit tests: - Some helper functions are added to the text fixture.

--- test/ara/diag/security_access_test.cpp
+++ test/ara/diag/security_access_test.cpp
@@ -9,86 +9,288 @@ namespace ara
         {
         private:
             static core::InstanceSpecifier mSpecifier;
-            static const ReentrancyType cReentrancy{ReentrancyType::kNot};
+            static const ReentrancyType cReentrancy;
+
+            const uint8_t cAttemptThreshold{1};
+            const int cExceededAttemptDelay{3600};
+
+            const std::string cEncryptorKey{"Encryptor"};
+            const std::string cAttemptThresholdKey{"AttemptThreshold"};
+            const std::string cExceededAttemptDelayKey{"ExceededAttemptDelay"};
 
         protected:
+            const uint8_t cSid{0x27};
+            const uint16_t cEncryptor{0x1234};
+
             SecurityAccess Service;
+            MetaInfo GeneralMetaInfo;
 
         public:
-            SecurityAccessTest() : Service{mSpecifier, cReentrancy}
+            SecurityAccessTest() : Service{mSpecifier, cReentrancy},
+                                   GeneralMetaInfo(Context::kDoIP)
             {
+                std::string _encryptorStr{std::to_string(cEncryptor)};
+                GeneralMetaInfo.SetValue(cEncryptorKey, _encryptorStr);
+
+                std::string _attemptThresholdStr{std::to_string(cAttemptThreshold)};
+                GeneralMetaInfo.SetValue(cAttemptThresholdKey, _attemptThresholdStr);
+
+                std::string _exceededAttemptDelayStr{std::to_string(cExceededAttemptDelay)};
+                GeneralMetaInfo.SetValue(cExceededAttemptDelayKey, _exceededAttemptDelayStr);
+            }
+
+            uint16_t GetSeed(uint8_t subFunction)
+            {
+                const size_t cSeedOffset{0};
+
+                std::vector<uint8_t> _securityAccessDataRecord;
+                CancellationHandler _cancellationHandler(false);
+
+                std::future<std::vector<uint8_t>> _resultByteArrFuture{
+                    Service.GetSeed(
+                        subFunction,
+                        _securityAccessDataRecord,
+                        GeneralMetaInfo,
+                        std::move(_cancellationHandler))};
+
+                std::vector<uint8_t> _resultByteArr{_resultByteArrFuture.get()};
+                size_t _offset{cSeedOffset};
+                auto _result{static_cast<uint16_t>(_resultByteArr.at(_offset)) << 8};
+                ++_offset;
+                _result |= _resultByteArr.at(_offset);
+
+                return _result;
+            }
+
+            bool TryGetNrc(const std::vector<uint8_t> &requestData, uint8_t &nrc)
+            {
+                const size_t cRejectedSidIndex{1};
+                const size_t cNrcIndex{2};
+
+                CancellationHandler _cancellationHandler(false);
+
+                std::future<OperationOutput> _responseFuture{
+                    Service.HandleMessage(
+                        requestData,
+                        GeneralMetaInfo,
+                        std::move(_cancellationHandler))};
+
+                OperationOutput _response{_responseFuture.get()};
+
+                uint8_t _sid{_response.responseData.at(cRejectedSidIndex)};
+                if (_sid == cSid)
+                {
+                    nrc = _response.responseData.at(cNrcIndex);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
         };
 
         core::InstanceSpecifier SecurityAccessTest::mSpecifier{"Instance0"};
-        const ReentrancyType SecurityAccessTest::cReentrancy;
+        const ReentrancyType SecurityAccessTest::cReentrancy{ReentrancyType::kNot};
 
-        TEST_F(SecurityAccessTest, HandleRequestSeedMessage)
+        TEST_F(SecurityAccessTest, IncorrectRequestScenario)
         {
-            const uint8_t cSid{0x27};
-            const size_t cSidIndex{1};
-            const uint8_t cNrc{0x12};
-            const size_t cNrcIndex{2};
+            const uint8_t cExpectedNrc{0x13};
+
+            uint8_t _actualNrc;
+            std::vector<uint8_t> _requestData{cSid};
+            bool _hasNrc{TryGetNrc(_requestData, _actualNrc)};
+
+            EXPECT_TRUE(_hasNrc);
+            EXPECT_EQ(cExpectedNrc, _actualNrc);
+        }
+
+        TEST_F(SecurityAccessTest, InvalidSubFunctionScenario)
+        {
+            const uint8_t cExpectedNrc{0x12};
             const uint8_t cSubFunction{0x43};
 
+            uint8_t _actualNrc;
             std::vector<uint8_t> _requestData{cSid, cSubFunction};
-            MetaInfo _metaInfo(Context::kDoIP);
+            bool _hasNrc{TryGetNrc(_requestData, _actualNrc)};
+
+            EXPECT_TRUE(_hasNrc);
+            EXPECT_EQ(cExpectedNrc, _actualNrc);
+        }
+
+        TEST_F(SecurityAccessTest, GetSeedMethod)
+        {
+            const uint8_t cSubFunction{0x01};
+
+            uint16_t _expectedResult{GetSeed(cSubFunction)};
+            EXPECT_GT(_expectedResult, 0);
+
+            uint16_t _actualResult{GetSeed(cSubFunction)};
+            EXPECT_EQ(_expectedResult, _actualResult);
+        }
+
+        TEST_F(SecurityAccessTest, IncorrectKeyLengthScenario)
+        {
+            const uint8_t cSubFunction{0x02};
+            const uint8_t cExpectedNrc{0x13};
+
+            uint8_t _actualNrc;
+            std::vector<uint8_t> _requestData{cSid, cSubFunction};
+            bool _hasNrc{TryGetNrc(_requestData, _actualNrc)};
+
+            EXPECT_TRUE(_hasNrc);
+            EXPECT_EQ(cExpectedNrc, _actualNrc);
+        }
+
+        TEST_F(SecurityAccessTest, InvalidCompareKeyMethod)
+        {
+            const uint8_t cSubFunction{0x01};
+            const uint8_t cKeyMsb{0x00};
+            const uint8_t cKeyLsb{0x00};
+            const KeyCompareResultType cExpectedResult{KeyCompareResultType::kKeyInvalid};
+
+            GetSeed(cSubFunction);
+
+            std::vector<uint8_t> _keyByteArr{cKeyMsb, cKeyMsb};
+            CancellationHandler _cancellationHandler(false);
+
+            std::future<KeyCompareResultType> _actualResultFuture{
+                Service.CompareKey(
+                    cSubFunction,
+                    _keyByteArr,
+                    GeneralMetaInfo,
+                    std::move(_cancellationHandler))};
+
+            KeyCompareResultType _actualResult{_actualResultFuture.get()};
+
+            EXPECT_EQ(cExpectedResult, _actualResult);
+        }
+
+        TEST_F(SecurityAccessTest, ValidCompareKeyMethod)
+        {
+            const uint8_t cSubFunction{0x01};
+            const KeyCompareResultType cExpectedResult{KeyCompareResultType::kKeyValid};
+
+            uint16_t _seed{GetSeed(cSubFunction)};
+
+            auto _key{static_cast<uint16_t>(_seed ^ cEncryptor)};
+            auto _keyMsb{static_cast<uint8_t>(_key >> 8)};
+            auto _keyLsb{static_cast<uint8_t>(_key)};
+            std::vector<uint8_t> _keyByteArr{_keyMsb, _keyLsb};
+
+            CancellationHandler _cancellationHandler(false);
+
+            std::future<KeyCompareResultType> _actualResultFuture{
+                Service.CompareKey(
+                    cSubFunction,
+                    _keyByteArr,
+                    GeneralMetaInfo,
+                    std::move(_cancellationHandler))};
+
+            KeyCompareResultType _actualResult{_actualResultFuture.get()};
+
+            EXPECT_EQ(cExpectedResult, _actualResult);
+        }
+
+        TEST_F(SecurityAccessTest, SequenceErrorScenario)
+        {
+            const uint8_t cExpectedNrc{0x24};
+            const uint8_t cSubFunction{0x02};
+            const uint8_t cKeyMsb{0x00};
+            const uint8_t cKeyLsb{0x00};
+
+            std::vector<uint8_t> _requestData{cSid, cSubFunction, cKeyMsb, cKeyLsb};
+            uint8_t _actualNrc;
+            bool _hasNrc{TryGetNrc(_requestData, _actualNrc)};
+
+            EXPECT_TRUE(_hasNrc);
+            EXPECT_EQ(cExpectedNrc, _actualNrc);
+        }
+
+        TEST_F(SecurityAccessTest, InvalidKeyScenrio)
+        {
+            const uint8_t cExpectedNrc{0x35};
+            const uint8_t cSubFunction{0x01};
+            const uint8_t cKeyMsb{0x00};
+            const uint8_t cKeyLsb{0x00};
+
+            GetSeed(cSubFunction);
+
+            auto _keyCompareSubFunction{static_cast<uint8_t>(cSubFunction + 1)};
+            std::vector<uint8_t> _requestData{cSid, _keyCompareSubFunction, cKeyMsb, cKeyLsb};
+            uint8_t _actualNrc;
+            bool _hasNrc{TryGetNrc(_requestData, _actualNrc)};
+
+            EXPECT_TRUE(_hasNrc);
+            EXPECT_EQ(cExpectedNrc, _actualNrc);
+        }
+
+        TEST_F(SecurityAccessTest, ExceededNumberOfAttemptsScenario)
+        {
+            const uint8_t cExpectedNrc{0x36};
+            const uint8_t cSubFunction{0x01};
+            const uint8_t cKeyMsb{0x00};
+            const uint8_t cKeyLsb{0x00};
+
+            GetSeed(cSubFunction);
+
+            auto _keyCompareSubFunction{static_cast<uint8_t>(cSubFunction + 1)};
+            std::vector<uint8_t> _requestData{cSid, _keyCompareSubFunction, cKeyMsb, cKeyLsb};
+            uint8_t _actualNrc;
+            TryGetNrc(_requestData, _actualNrc);
+            bool _hasNrc{TryGetNrc(_requestData, _actualNrc)};
+
+            EXPECT_TRUE(_hasNrc);
+            EXPECT_EQ(cExpectedNrc, _actualNrc);
+        }
+
+        TEST_F(SecurityAccessTest, ValidKeyScenrio)
+        {
+            const uint8_t cSubFunction{0x01};
+            const size_t cSidIndex{0};
+            const size_t cSubFunctionIndex{1};
+
+            uint16_t _seed{GetSeed(cSubFunction)};
+            auto _key{static_cast<uint16_t>(_seed ^ cEncryptor)};
+            auto _keyMsb{static_cast<uint8_t>(_key >> 8)};
+            auto _keyLsb{static_cast<uint8_t>(_key)};
+
+            auto _keyCompareSubFunction{static_cast<uint8_t>(cSubFunction + 1)};
+            std::vector<uint8_t> _requestData{cSid, _keyCompareSubFunction, _keyMsb, _keyLsb};
             CancellationHandler _cancellationHandler(false);
 
             std::future<OperationOutput> _responseFuture{
                 Service.HandleMessage(
-                    _requestData,
-                    _metaInfo,
-                    std::move(_cancellationHandler))};
+                    _requestData, GeneralMetaInfo, std::move(_cancellationHandler))};
 
             OperationOutput _response{_responseFuture.get()};
 
             uint8_t _sid{_response.responseData.at(cSidIndex)};
             EXPECT_EQ(cSid, _sid);
 
-            uint8_t _nrc{_response.responseData.at(cNrcIndex)};
-            EXPECT_EQ(cNrc, _nrc);
+            uint8_t _subFunction{_response.responseData.at(cSubFunctionIndex)};
+            EXPECT_EQ(_keyCompareSubFunction, _subFunction);
         }
 
-        TEST_F(SecurityAccessTest, GetSeedMethod)
+        TEST_F(SecurityAccessTest, UnlockedLevelSeedScenario)
         {
-            const size_t cSeedOffset{0};
             const uint8_t cSubFunction{0x01};
+            const uint8_t cExpectedResult{0};
 
-            std::vector<uint8_t> _securityAccessDataRecord;
-            MetaInfo _metaInfo(Context::kDoIP);
+            uint16_t _seed{GetSeed(cSubFunction)};
+            auto _key{static_cast<uint16_t>(_seed ^ cEncryptor)};
+            auto _keyMsb{static_cast<uint8_t>(_key >> 8)};
+            auto _keyLsb{static_cast<uint8_t>(_key)};
+
+            auto _keyCompareSubFunction{static_cast<uint8_t>(cSubFunction + 1)};
+            std::vector<uint8_t> _requestData{cSid, _keyCompareSubFunction, _keyMsb, _keyLsb};
             CancellationHandler _cancellationHandler(false);
 
-            std::future<std::vector<uint8_t>> _seedByteArrFuture{
-                Service.GetSeed(
-                    cSubFunction,
-                    _securityAccessDataRecord,
-                    _metaInfo,
-                    std::move(_cancellationHandler))};
+            Service.HandleMessage(_requestData, GeneralMetaInfo, std::move(_cancellationHandler));
 
-            std::vector<uint8_t> _seedByteArr{_seedByteArrFuture.get()};
-            size_t _offset{cSeedOffset};
-            auto _expectedResult{static_cast<uint16_t>(_seedByteArr.at(_offset)) << 8};
-            ++_offset;
-            _expectedResult |= _seedByteArr.at(_offset);
+            uint16_t _actualResult{GetSeed(cSubFunction)};
 
-            EXPECT_GT(_expectedResult, 0);
-
-            CancellationHandler _newCancellationHandler(false);
-            _seedByteArrFuture =
-                Service.GetSeed(
-                    cSubFunction,
-                    _securityAccessDataRecord,
-                    _metaInfo,
-                    std::move(_newCancellationHandler));
-
-            _seedByteArr = _seedByteArrFuture.get();
-            _offset = cSeedOffset;
-            auto _actualResult{static_cast<uint16_t>(_seedByteArr.at(_offset)) << 8};
-            ++_offset;
-            _actualResult |= _seedByteArr.at(_offset);
-
-            EXPECT_EQ(_expectedResult, _actualResult);
+            EXPECT_EQ(cExpectedResult, _actualResult);
         }
     }
 }
