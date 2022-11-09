b5db94fa9b8e2b9cf98fc181f3e3dc70c909b3a9
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2022-11-09 21:22:24 +0100

    :shower: Refactor ModelledProcess interface: - The class is now responsilbe to create its own thread for the main block; - Initialize and Terminate methods are no longer abstract; - However, the main block logic is moved to the Main abstract method; - Initialization now supports arguments; - Class dtor is no more exception free due to getting a future value as exit code.

--- src/ara/exec/helper/modelled_process.h
+++ src/ara/exec/helper/modelled_process.h
@@ -1,6 +1,11 @@
 #ifndef MODELLED_PROCESS_H
 #define MODELLED_PROCESS_H
 
+#include <atomic>
+#include <future>
+#include <map>
+#include <string>
+
 namespace ara
 {
     namespace exec
@@ -10,14 +15,33 @@ namespace ara
             /// @brief A class that models an instance of an Adaptive (Platform) Application executable
             class ModelledProcess
             {
+            private:
+                std::atomic_bool mCancellationToken;
+                std::future<int> mExitCode;
+
+            protected:
+                ModelledProcess() noexcept;
+
+                /// @brief Main running block of the process
+                /// @param arguments Initialization arguments keys and their corresponding values
+                /// @return Exit code
+                /// @note Exit code zero means the graceful shutdown of the process.
+                virtual int Main(
+                    const std::atomic_bool *cancellationToken,
+                    const std::map<std::string, std::string> &arguments) = 0;
+
             public:
-                /// @brief Initialize the process model
-                virtual void Initialize() = 0;
+                /// @brief Initialize the process model to run the main block
+                /// @param arguments Initialization arguments keys and their corresponding values
+                /// @see Main(const std::map<std::string, std::string> &)
+                void Initialize(const std::map<std::string, std::string> &arguments);
 
                 /// @brief Terminate the process model
-                virtual void Terminate() = 0;
+                /// @return Returned exit code from the main running block
+                /// @note The caller will blocked until the termination be finished.
+                int Terminate();
 
-                virtual ~ModelledProcess() noexcept = default;
+                virtual ~ModelledProcess();
             };
         }
     }
