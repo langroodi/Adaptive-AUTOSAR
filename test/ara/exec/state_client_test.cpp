9c573ca662146a53e1711af584549096d96ab6b1
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2022-12-23 13:02:56 +0100

    :microscope: Extend StateClient unit tests

--- test/ara/exec/state_client_test.cpp
+++ test/ara/exec/state_client_test.cpp
@@ -22,6 +22,16 @@ namespace ara
             StateClientTest() : Client{mEmptyCallback, &mRpcClient}
             {
             }
+
+            void SetBypass(bool bypass)
+            {
+                mRpcClient.SetBypass(bypass);
+            }
+
+            void SetRpcPayload(std::vector<uint8_t> &&rpcPayload)
+            {
+                mRpcClient.SetRpcPayload(std::move(rpcPayload));
+            }
         };
 
         TEST_F(StateClientTest, SetStateMethod)
@@ -33,7 +43,7 @@ namespace ara
             auto _functionGroupState =
                 FunctionGroupState::Create(_functionGroup, cState).Value();
 
-            std::future<void> _succeed{Client.SetState(_functionGroupState)};
+            std::shared_future<void> _succeed{Client.SetState(_functionGroupState)};
             EXPECT_TRUE(_succeed.valid());
 
             std::future_status _status{_succeed.wait_for(cTimeout)};
@@ -43,12 +53,64 @@ namespace ara
 
         TEST_F(StateClientTest, GetInitialMachineStateTransitionResultMehod)
         {
-            std::future<void> _succeed{
+            std::shared_future<void> _succeed{
                 Client.GetInitialMachineStateTransitionResult()};
 
             std::future_status _status{_succeed.wait_for(cTimeout)};
             EXPECT_NE(_status, cTimeoutStatus);
             EXPECT_NO_THROW(_succeed.get());
+        }
+
+        TEST_F(StateClientTest, RedundantRequestScenario)
+        {
+            SetBypass(true);
+            std::shared_future<void> _succeed{
+                Client.GetInitialMachineStateTransitionResult()};
+
+            SetBypass(false);
+            std::shared_future<void> _redundantSucceed{
+                Client.GetInitialMachineStateTransitionResult()};
+
+            std::future_status _status{_succeed.wait_for(cTimeout)};
+            EXPECT_NE(_status, cTimeoutStatus);
+            EXPECT_THROW(_succeed.get(), ExecException);
+
+            _status = _redundantSucceed.wait_for(cTimeout);
+            EXPECT_NE(_status, cTimeoutStatus);
+            EXPECT_NO_THROW(_redundantSucceed.get());
+        }
+
+        TEST_F(StateClientTest, TimeoutScenario)
+        {
+            const std::chrono::seconds _timeout(1);
+            SetBypass(true);
+            std::shared_future<void> _succeed{
+                Client.GetInitialMachineStateTransitionResult()};
+
+            std::future_status _status{_succeed.wait_for(_timeout)};
+            EXPECT_EQ(_status, cTimeoutStatus);
+        }
+
+        TEST_F(StateClientTest, CorruptedResponseScenario)
+        {
+            SetRpcPayload(std::vector<uint8_t>({0x00}));
+            std::shared_future<void> _succeed{
+                Client.GetInitialMachineStateTransitionResult()};
+
+            std::future_status _status{_succeed.wait_for(cTimeout)};
+            EXPECT_NE(_status, cTimeoutStatus);
+            EXPECT_THROW(_succeed.get(), ExecException);
+        }
+
+        TEST_F(StateClientTest, RequestFailureScenario)
+        {
+            SetRpcPayload(std::vector<uint8_t>({0, 0, 0, 1}));
+            std::shared_future<void> _succeed{
+                Client.GetInitialMachineStateTransitionResult()};
+
+            std::future_status _status{_succeed.wait_for(cTimeout)};
+            EXPECT_NE(_status, cTimeoutStatus);
+            EXPECT_THROW(_succeed.get(), ExecException);
         }
 
         TEST_F(StateClientTest, GetExecutionErrorMethod)
