7274c84b3f8a8803cf4737a71e6181ad6d8673b1
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2022-12-30 15:36:03 +0100

    :beer: Enable debugging the main application: - Inject the default config file path if no argument is provided by the debugger.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 int main(int argc, char *argv[])
 {
+    const std::string cDefaultConfigFile{"../configuration/execution_manifest.arxml"};
     const std::string cConfigArgument{"config"};
     const int cConfigArgumentIndex{1};
 
@@ -11,6 +12,10 @@ int main(int argc, char *argv[])
     {
         std::string _configFilepath{argv[cConfigArgumentIndex]};
         _arguments[cConfigArgument] = _configFilepath;
+    }
+    else
+    {
+        _arguments[cConfigArgument] = cDefaultConfigFile;
     }
 
     application::platform::ExecutionManagement _executionManagement;
