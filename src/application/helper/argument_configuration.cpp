05ee2269163d2f7a14e11bc9f3cd78152214b607
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2023-02-18 13:57:01 +0100

    :beer: Introduce ArgumentConfiguratuin helper class: - The class helps to manage the arguments passed to the main routine.

---
+++ src/application/helper/argument_configuration.cpp
@@ -0,0 +1,113 @@
+#include <termios.h>
+#include <unistd.h>
+#include <iostream>
+#include "./argument_configuration.h"
+
+namespace application
+{
+    namespace helper
+    {
+        const std::string ArgumentConfiguration::cConfigArgument{"config"};
+        const std::string ArgumentConfiguration::cEvConfigArgument{"evconfig"};
+        const std::string ArgumentConfiguration::cDmConfigArgument{"dmconfig"};
+        const std::string ArgumentConfiguration::cApiKeyArgument{"vccapikey"};
+        const std::string ArgumentConfiguration::cBearerTokenArgument{"bearertoken"};
+
+        ArgumentConfiguration::ArgumentConfiguration(
+            int argc,
+            char *argv[],
+            std::string defaultConfigFile,
+            std::string extendedVehicleConfigFile,
+            std::string diagnosticManagerConfigFile)
+        {
+            const int cConfigArgumentIndex{1};
+            const int cEvConfigArgumentIndex{2};
+            const int cDmConfigArgumentIndex{3};
+
+            if (argc > cDmConfigArgumentIndex)
+            {
+                std::string _configFilepath{argv[cConfigArgumentIndex]};
+                mArguments[cConfigArgument] = _configFilepath;
+
+                std::string _evConfigFilepath{argv[cEvConfigArgumentIndex]};
+                mArguments[cEvConfigArgument] = _evConfigFilepath;
+
+                std::string _dmConfigFilepath{argv[cDmConfigArgumentIndex]};
+                mArguments[cDmConfigArgument] = _dmConfigFilepath;
+            }
+            else
+            {
+                mArguments[cConfigArgument] = defaultConfigFile;
+                mArguments[cEvConfigArgument] = extendedVehicleConfigFile;
+                mArguments[cDmConfigArgument] = diagnosticManagerConfigFile;
+            }
+        }
+
+        bool ArgumentConfiguration::trySetEchoMode(bool enabled)
+        {
+            const int cSuccessfulCode{0};
+
+            struct termios _tty;
+            // Read the console input file descriptor attributes
+            int _successful{tcgetattr(STDIN_FILENO, &_tty)};
+
+            if (_successful == cSuccessfulCode)
+            {
+                if (enabled)
+                {
+                    // Enable the echo attribute flag
+                    _tty.c_lflag |= ECHO;
+                }
+                else
+                {
+                    // Disable the echo attribute flag
+                    _tty.c_lflag &= ~ECHO;
+                }
+
+                // Appy the modified attributes to the file descriptor immediately
+                _successful = tcsetattr(STDIN_FILENO, TCSANOW, &_tty);
+            }
+
+            bool _result{_successful == cSuccessfulCode};
+            return _result;
+        }
+
+        bool ArgumentConfiguration::tryAskSafely(
+            std::string message, std::string argumentKey)
+        {
+            std::cout << message << std::endl;
+            bool _result{trySetEchoMode(false)};
+
+            if (_result)
+            {
+                std::string userInput;
+                std::cin >> userInput;
+                mArguments[argumentKey] = userInput;
+
+                _result = trySetEchoMode(true);
+                if (!_result)
+                {
+                    // Revert the arguments dictionary to the state before the function call
+                    mArguments.erase(argumentKey);
+                }
+            }
+
+            return _result;
+        }
+
+        const std::map<std::string, std::string> &ArgumentConfiguration::GetArguments() const noexcept
+        {
+            return mArguments;
+        }
+
+        bool ArgumentConfiguration::TryAskingVccApiKey(std::string message)
+        {
+            return tryAskSafely(message, cApiKeyArgument);
+        }
+
+        bool ArgumentConfiguration::TryAskingBearToken(std::string message)
+        {
+            return tryAskSafely(message, cBearerTokenArgument);
+        }
+    }
+}
---
+++ src/application/helper/argument_configuration.h
@@ -0,0 +1,65 @@
+#ifndef ARGUMENT_CONFIGURATION_H
+#define ARGUMENT_CONFIGURATION_H
+
+#include <map>
+#include <string>
+
+namespace application
+{
+    namespace helper
+    {
+        /// @brief A helper class to manage the arguments passed to the main application
+        class ArgumentConfiguration
+        {
+        private:
+            std::map<std::string, std::string> mArguments;
+
+            bool trySetEchoMode(bool enabled);
+            bool tryAskSafely(std::string message, std::string argumentKey);
+
+        public:
+            /// @brief Execution manifest filename argument key
+            static const std::string cConfigArgument;
+            /// @brief Extended Vehicle AA manifest filename argument key
+            static const std::string cEvConfigArgument;
+            /// @brief Diagnostic Manager manifest filename argument key
+            static const std::string cDmConfigArgument;
+            /// @brief VCC API key argument key
+            static const std::string cApiKeyArgument;
+            /// @brief OAuth 2.0 bearer token argument key
+            static const std::string cBearerTokenArgument;
+
+            /// @brief Constructor
+            /// @param argc Argument count
+            /// @param argv Passed arguments
+            /// @param defaultConfigFile Default execution manifest file path
+            /// @param extendedVehicleConfigFile Default Extended Vehicle AA manifest file path
+            /// @param diagnosticManagerConfigFile Default DM manifest file path
+            ArgumentConfiguration(
+                int argc,
+                char *argv[],
+                std::string defaultConfigFile = "../configuration/execution_manifest.arxml",
+                std::string extendedVehicleConfigFile = "../configuration/extended_vehicle_manifest.arxml",
+                std::string diagnosticManagerConfigFile = "../configuration/diagnostic_manager_manifest.arxml");
+            ArgumentConfiguration() = delete;
+
+            /// @brief Arguments property getter
+            /// @return All the parsed and/or set arguments
+            const std::map<std::string, std::string> &GetArguments() const noexcept;
+
+            /// @brief Try asking the user to enter the VCC API key safely
+            /// @param message Message to be shown on the console to ask the API key
+            /// @return True if the API key is set correctly; otherwise false
+            bool TryAskingVccApiKey(
+                std::string message = "Please enter the VCC API key:");
+
+            /// @brief Try asking the user to enter the OAuth 2.0 bearer token safely
+            /// @param message Message to be shown on the console to ask the  bearer token
+            /// @return True if the bearer token is set correctly; otherwise false
+            bool TryAskingBearToken(
+                std::string message = "Please enter the OAuth 2.0 bearer token:");
+        };
+    }
+}
+
+#endif
