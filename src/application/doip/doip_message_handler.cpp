f44b877967b154d263165d82a5909ea5f1c7153f
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2023-03-21 18:41:32 +0100

    :beer: Add DoipMessageHandler class: - The class is added to route DoIP messages to ReadDataById UDS

---
+++ src/application/doip/doip_message_handler.cpp
@@ -0,0 +1,64 @@
+#include <doiplib/diag_message_ack.h>
+#include "./doip_message_handler.h"
+
+namespace application
+{
+    namespace doip
+    {
+        DoipMessageHandler::DoipMessageHandler(
+            helper::CurlWrapper *curl,
+            std::string resourcesUrl,
+            uint8_t protocolVersion) : mService(curl, resourcesUrl),
+                                       cProtocolVersion{protocolVersion}
+        {
+            mRouter.AddService(&mService);
+        }
+
+        DoipLib::Message *DoipMessageHandler::GetMessage()
+        {
+            return static_cast<DoipLib::Message *>(&mRequest);
+        }
+
+        bool DoipMessageHandler::TryHandle(
+            const DoipLib::Message *request,
+            std::vector<uint8_t> &response) const
+        {
+            auto _diagMessage{
+                dynamic_cast<const DoipLib::DiagMessage *>(request)};
+
+            if (_diagMessage)
+            {
+                std::vector<uint8_t> _userData;
+                _diagMessage->GetUserData(_userData);
+                ara::diag::MetaInfo _metaInfo(ara::diag::Context::kDoIP);
+                ara::diag::CancellationHandler _cancellationHandler(false);
+
+                try
+                {
+                    std::future<ara::diag::OperationOutput> _operationOutput{
+                        mRouter.Route(
+                            _userData, _metaInfo, std::move(_cancellationHandler))};
+
+                    /// @note Previous message field is used for the UDS response data
+                    DoipLib::DiagMessageAck _diagMessageAck(
+                        cProtocolVersion,
+                        _diagMessage->GetSourceAddress(),
+                        _diagMessage->GetTargetAddress(),
+                        _operationOutput.get().responseData);
+
+                    _diagMessageAck.Serialize(response);
+
+                    return true;
+                }
+                catch (const std::invalid_argument)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ src/application/doip/doip_message_handler.h
@@ -0,0 +1,42 @@
+#ifndef DOIP_MESSAGE_HANDLER_H
+#define DOIP_MESSAGE_HANDLER_H
+
+#include <doiplib/message_handler.h>
+#include <doiplib/diag_message.h>
+#include "../helper/read_data_by_identifier.h"
+#include "../../ara/diag/routing/uds_service_router.h"
+
+namespace application
+{
+    /// @brief Namespace that contains helpers which utilize applications for DoIP communication
+    namespace doip
+    {
+        /// @brief A class to handle UDS messages via DoIP
+        class DoipMessageHandler : public DoipLib::MessageHandler
+        {
+        private:
+            DoipLib::DiagMessage mRequest;
+            ara::diag::routing::UdsServiceRouter mRouter;
+            helper::ReadDataByIdentifier mService;
+            const uint8_t cProtocolVersion;
+
+        public:
+            /// @brief Constructor
+            /// @param curl Configured CURL instance for RESTful communication
+            /// @param resourcesUrl Connected vehicle resources access RESTful URL
+            /// @param protocolVersion DoIP protocol version
+            DoipMessageHandler(
+                helper::CurlWrapper *curl,
+                std::string resourcesUrl,
+                uint8_t protocolVersion);
+
+            DoipLib::Message *GetMessage() override;
+
+            bool TryHandle(
+                const DoipLib::Message *request,
+                std::vector<uint8_t> &response) const override;
+        };
+    }
+}
+
+#endif
