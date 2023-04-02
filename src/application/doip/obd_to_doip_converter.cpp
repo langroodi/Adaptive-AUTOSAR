189bde8e84f2fdef0970efa7d1706e9a43f2de1f
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2023-04-02 08:46:26 +0200

    :beer: Add ObdToDoipConverter class: - Update to the lastest OBD-II Emulator library version.

---
+++ src/application/doip/obd_to_doip_converter.cpp
@@ -0,0 +1,312 @@
+#include "./obd_to_doip_converter.h"
+
+namespace application
+{
+    namespace doip
+    {
+        const uint8_t ObdToDoipConverter::cService;
+        const uint8_t ObdToDoipConverter::cSid;
+
+        ObdToDoipConverter::ObdToDoipConverter(
+            AsyncBsdSocketLib::Poller *poller,
+            std::string ipAddress,
+            uint16_t port,
+            uint8_t protocolVersion) : ObdService(cService),
+                                       mClient(
+                                           poller,
+                                           ipAddress,
+                                           port,
+                                           protocolVersion,
+                                           std::bind(&ObdToDoipConverter::onUdsDataReceived, this, std::placeholders::_1))
+        {
+        }
+
+        bool ObdToDoipConverter::tryParseUdsData(
+            std::vector<uint8_t> &&udsResponse,
+            std::vector<uint8_t> &obdData)
+        {
+            const size_t cSidIndex{0};
+            const size_t cDataOffset{3};
+
+            if (udsResponse.size() < cDataOffset)
+            {
+                return false;
+            }
+
+            const uint8_t cReceivedSid{udsResponse.at(cSidIndex)};
+            if (cReceivedSid == cSid)
+            {
+                // This is a NACK
+                return false;
+            }
+
+            obdData =
+                std::vector<uint8_t>(
+                    udsResponse.begin() + cDataOffset, udsResponse.end());
+
+            return true;
+        }
+
+        void ObdToDoipConverter::onUdsDataReceived(
+            std::vector<uint8_t> &&receivedUdsData) const
+        {
+            std::vector<uint8_t> _obdData;
+
+            bool cSuccessful{
+                tryParseUdsData(std::move(receivedUdsData), _obdData)};
+
+            if (cSuccessful && mCallback)
+            {
+                mCallback(std::move(_obdData));
+            }
+        }
+
+        bool ObdToDoipConverter::trySendAsDoip(uint8_t pid)
+        {
+            const uint8_t cDidMsb{0xf5};
+
+            std::vector<uint8_t> _udsPayload{cSid, cDidMsb, pid};
+            bool cResult{mClient.TrySendDiagMessage(std::move(_udsPayload))};
+
+            return cResult;
+        }
+
+        bool ObdToDoipConverter::TryGetResponse(
+            const std::vector<uint8_t> &pid,
+            std::vector<uint8_t> &response) const
+        {
+            return false;
+        }
+
+        bool ObdToDoipConverter::TryGetResponseAsync(
+            const std::vector<uint8_t> &pid,
+            std::function<void(std::vector<uint8_t> &&)> &&callback)
+        {
+            const size_t cPidIndex{0};
+            bool _result{true};
+            uint8_t _queriedPid{pid.at(cPidIndex)};
+
+            switch (_queriedPid)
+            {
+            case 0x00:
+                // Most significant 4 bits                0000 0000 0000 0001 1111 1111 1111 1112
+                // Least significant 4 bits               1234 5678 9abc def0 1234 5678 9abc def0
+                // Supported PIDs within [0x01:0x20] are {0011 1110 0111 1111 1111 0000 0001 0101}.
+                callback({0x3e, 0x7f, 0xf0, 0x15});
+                break;
+            case 0x03:
+                // The first fuel system state is closed loop using the oxygen sensor feedback.
+                // The second fuel system does not exist.
+                callback({0x02, 0x00});
+                break;
+            case 0x04:
+                // The engine load is 20%,
+                callback({0x33});
+                break;
+            case 0x06:
+                // Short Term Fuel Trim (STFT) of bank 1 is ~10%.
+                callback({0x8c});
+                break;
+            case 0x07:
+                // Long Term Fuel Trim (LTFT) of bank 1 is 0%.
+                callback({0x80});
+                break;
+            case 0x0a:
+                // The fuel gauge pressure is 240 kPa.
+                callback({0x50});
+                break;
+            case 0x0b:
+                // The intake manifold absolute pressure  is 75 kPa.
+                callback({0x4b});
+                break;
+            case 0x0c:
+                // The engine speed is 2048 rpm.
+                callback({0x20, 0x00});
+                break;
+            case 0x0e:
+                // Timing advance before TDC is 9°.
+                callback({0x92});
+                break;
+            case 0x0f:
+                // Intake air temperature is 40 °C.
+                callback({0x50});
+                break;
+            case 0x10:
+                // Mass air flow rate is 5.2 g/s.
+                callback({0x02, 0x08});
+                break;
+            case 0x11:
+                // Throttle position is 20%.
+                callback({0x33});
+                break;
+            case 0x12:
+                // Secondary air monitor is off.
+                callback({0x04});
+                break;
+            case 0x13:
+                // There is only one oxygen sensor in bank 1.
+                callback({0x01});
+                break;
+            case 0x14:
+                // The oxygen sensor voltage is 1 V and it is not used to calculate the STFT.
+                callback({0xc8, 0xff});
+                break;
+            case 0x1c:
+                // Only OBD-II is supported.
+                callback({0x01});
+                break;
+            case 0x1e:
+                // Power Take Off (PTO) is inactive.
+                callback({0x00});
+                break;
+            case 0x20:
+                // Most significant 4 bits                2222 2222 2222 2223 3333 3333 3333 3334
+                // Least significant 4 bits               1234 5678 9abc def0 1234 5678 9abc def0
+                // Supported PIDs within [0x21:0x40] are {1101 0000 0000 0010 0111 0000 0001 0001}.
+                callback({0xd0, 0x02, 0x70, 0x11});
+                break;
+            case 0x21:
+                // Distance traveled with malfunction indicator lamp (MIL) on is 0 km.
+                callback({0x00, 0x00});
+                break;
+            case 0x22:
+                // Fuel rail absolute pressure ~515 kPa.
+                callback({0x19, 0x78});
+                break;
+            case 0x24:
+                // Oxygen sensor 1 lambda is 1 and its voltage is 1 V.
+                callback({0x80, 0x00, 0x20, 0x00});
+                break;
+            case 0x32:
+                // EVAP canister pressure is 6.8 kPa.
+                callback({0x6a, 0x40});
+                break;
+            case 0x33:
+                // Absolute barometric pressure is 101 kPa.
+                callback({0x65});
+                break;
+            case 0x34:
+                // Oxygen sensor 1 lambda is 1 and its current is 100 mA.
+                callback({0x80, 0x00, 0xe4, 0x00});
+                break;
+            case 0x3c:
+                // Catalyst temperature of bank 1 read from sesnor 1 is 600 °C.
+                callback({0x19, 0x00});
+                break;
+            case 0x40:
+                // Most significant 4 bits                4444 4444 4444 4445 5555 5555 5555 5556
+                // Least significant 4 bits               1234 5678 9abc def0 1234 5678 9abc def0
+                // Supported PIDs within [0x41:0x60] are {0101 1100 0000 1011 1011 0000 1101 0101}.
+                callback({0x5c, 0x0b, 0xb0, 0xd5});
+                break;
+            case 0x42:
+                // ECU voltage is 12 V.
+                callback({0x2e, 0xe0});
+                break;
+            case 0x44:
+                // Lambda is 1.
+                callback({0x80, 0x00});
+                break;
+            case 0x45:
+                // Relative throttle is 20%.
+                callback({0x33});
+                break;
+            case 0x4d:
+                // Time running with MIL on is 0 min.
+                callback({0x00, 0x00});
+                break;
+            case 0x4f:
+                // Maximum lambda is 2,
+                // maximum oxygen sensor voltage is 8 V,
+                // maximum oxygen sesnor current is 128 mA, and
+                // maximum intake manifold absolute pressure is 250 kPa.
+                callback({0x02, 0x08, 0x80, 0x19});
+                break;
+            case 0x50:
+                // Maximum mass air flow rate is 650 g/s.
+                callback({0x41, 0x00, 0x00, 0x00});
+                break;
+            case 0x51:
+                // The fuel type is gasoline.
+                callback({0x01});
+                break;
+            case 0x53:
+                // EVAP canister absolute pressure is 107.8 kPa.
+                callback({0x54, 0x38});
+                break;
+            case 0x54:
+                // EVAP canister gauge pressure is 6.8 kPa.
+                callback({0x1a, 0x90});
+                break;
+            case 0x59:
+                // Fuel rail absolute pressure is 200 MPa.
+                callback({0x4e, 0x20});
+                break;
+            case 0x5a:
+                // Relative accelerator pedal position is 20%.
+                callback({0x33});
+                break;
+            case 0x5c:
+                // Oil temperature is 170 °C.
+                callback({0xd2});
+                break;
+            case 0x60:
+                // Most significant 4 bits                6666 6666 6666 6667 7777 7777 7777 7778
+                // Least significant 4 bits               1234 5678 9abc def0 1234 5678 9abc def0
+                // Supported PIDs within [0x61:0x80] are {1110 0011 0000 0000 0000 0000 0000 0001}.
+                callback({0xe3, 0x00, 0x00, 0x01});
+                break;
+            case 0x61:
+                // Driver demanded torque is 20%.
+                callback({0x91});
+                break;
+            case 0x62:
+                // Actual engine torque is 20%.
+                callback({0x91});
+                break;
+            case 0x63:
+                // Engine reference torque is 256 N.m.
+                callback({0x01, 0x00});
+                break;
+            case 0x67:
+                // Only sensor 1 is supported which measures coolant temperature at 70 °C.
+                callback({0x01, 0x6e, 0x00});
+                break;
+            case 0x68:
+                // Only sensor 1 is supported which measures intake air temperature at 40 °C.
+                callback({0x01, 0x50, 0x00});
+                break;
+            case 0x80:
+                // Most significant 4 bits                8888 8888 8888 8889 9999 9999 9999 999a
+                // Least significant 4 bits               1234 5678 9abc def0 1234 5678 9abc def0
+                // Supported PIDs within [0x81:0xa0] are {0000 0000 0000 0000 0000 0000 0000 0001}.
+                callback({0x00, 0x00, 0x00, 0x01});
+                break;
+            case 0xa0:
+                // Most significant 4 bits                aaaa aaaa aaaa aaab bbbb bbbb bbbb bbbc
+                // Least significant 4 bits               1234 5678 9abc def0 1234 5678 9abc def0
+                // Supported PIDs within [0xa1:0xc0] are {0000 0100 0000 0000 0000 0000 0000 0000}.
+                callback({0x04, 0x00, 0x00, 0x00});
+                break;
+            case 0x0d:
+            case 0x2f:
+            case 0x46:
+            case 0x5e:
+            case 0x05:
+            case 0xa6:
+                // Set the callback if it has not been set before
+                // It is assumed that the callback be the same for different PIDs
+                if (!mCallback)
+                {
+                    mCallback = std::move(callback);
+                }
+                _result = trySendAsDoip(_queriedPid);
+                break;
+            default:
+                _result = false;
+            }
+
+            return _result;
+        }
+    }
+}
---
+++ src/application/doip/obd_to_doip_converter.h
@@ -0,0 +1,56 @@
+#ifndef OBD_TO_DOIP_CONVERTER_H
+#define OBD_TO_DOIP_CONVERTER_H
+
+#include <obdemulator/obd_service.h>
+#include "./doip_client.h"
+
+namespace application
+{
+    namespace doip
+    {
+        /// @brief A OBD service class to handle OBD queries asynchronously
+        /// @details The class based on the queried PID may convert the query to a UDS message and send it via DoIP to a UDS server.
+        class ObdToDoipConverter : public ObdEmulator::ObdService
+        {
+        private:
+            // Show current data mode
+            static const uint8_t cService{0x01};
+            static const uint8_t cSid{0x22};
+
+            DoipClient mClient;
+            std::function<void(std::vector<uint8_t> &&)> mCallback;
+
+            static bool tryParseUdsData(
+                std::vector<uint8_t> &&udsResponse,
+                std::vector<uint8_t> &obdData);
+
+            void onUdsDataReceived(
+                std::vector<uint8_t> &&receivedUdsData) const;
+
+            bool trySendAsDoip(uint8_t pid);
+
+        public:
+            /// @brief Constructor
+            /// @param poller Global poller for network communication
+            /// @param ipAddress DoIP server IPv4 address
+            /// @param port DoIP server TCP listening port number
+            /// @param protocolVersion DoIP protocol version
+            /// @throws std::runtime_error Throws when the DoIP client initialization failed
+            ObdToDoipConverter(
+                AsyncBsdSocketLib::Poller *poller,
+                std::string ipAddress,
+                uint16_t port,
+                uint8_t protocolVersion);
+
+            bool TryGetResponse(
+                const std::vector<uint8_t> &pid,
+                std::vector<uint8_t> &response) const override;
+
+            bool TryGetResponseAsync(
+                const std::vector<uint8_t> &pid,
+                std::function<void(std::vector<uint8_t> &&)> &&callback) override;
+        };
+    }
+}
+
+#endif
